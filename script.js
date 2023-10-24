19afac8044d03ea48d0d870696d00a57c130e098
Jekoff <jlanloze@gmail.com>
2023-10-24 23:56:33 +0200

    commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', calculateTime)
+window.addEventListener('load', calculateTime);
 
-function calculateTime(){
+function calculateTime() {
     var date = new Date();
     var dayNumber = date.getDay();
     var hour = date.getHours();
-    var minute = minutes = date.getMinutes();
+    var minute = date.getMinutes();
     var ampm = hour >= 12 ? 'PM' : 'AM';
     var daysNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
     hour = hour % 12;
-    hour = hour ? 12 : 12;
+    hour = hour === 0 ? 12 : hour;
     hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
 
@@ -18,6 +18,5 @@ function calculateTime(){
     document.getElementById('minute').innerHTML = minute;
     document.getElementById('ampm').innerHTML = ampm;
 
-    setTimeout(calculateTime, 200;)
-
+    setTimeout(calculateTime, 200);
 }
