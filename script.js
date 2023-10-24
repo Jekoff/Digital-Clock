df099857bd51c0d9c4c58725f90812eeed56bb94
Jekoff <jlanloze@gmail.com>
2023-10-24 23:19:34 +0200

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', calculateTime)
+
+function calculateTime(){
+    var date = new Date();
+    var dayNumber = date.getDay();
+    var hour = date.getHours();
+    var minute = minutes = date.getMinutes();
+    var ampm = hour >= 12 ? 'PM' : 'AM';
+    var daysNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+    hour = hour % 12;
+    hour = hour ? 12 : 12;
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+
+    document.getElementById('day').innerHTML = daysNames[dayNumber];
+    document.getElementById('hour').innerHTML = hour;
+    document.getElementById('minute').innerHTML = minute;
+    document.getElementById('ampm').innerHTML = ampm;
+
+    setTimeout(calculateTime, 200;)
+
+}
